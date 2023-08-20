54175aa6f3dd16dbb1361ce21c762f201687c9b4
Nukambe <chappellwesley@gmail.com>
2023-08-20 19:55:44 -0400

    completed phase 1

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const apiRouter = require('./api');
+
+
 router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
     res.cookie("XSRF-TOKEN", csrfToken);
@@ -8,5 +11,7 @@ router.get("/api/csrf/restore", (req, res) => {
         'XSRF-Token': csrfToken
     });
 });
+
+router.use('/api', apiRouter);
 
 module.exports = router;
